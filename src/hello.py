de0f3ebd53965ba84993cb7e7b612215a3448f86
Qingshuo SONG <songqsh@gmail.com>
2022-03-06 20:57:07 -0500

    Revert "Update hello.py"
    This reverts commit 8cbe7fc11790e197d31e3718173538402c330e38.

--- src/hello.py
+++ src/hello.py
@@ -4,5 +4,4 @@ import math
 a = math.sin(math.pi)
 
 print(f"hello, the world! here is sin(pi) computed by python: {a}")
-print(f'this will be added temporarily and will be deleted later by using reverting option in github')
 
