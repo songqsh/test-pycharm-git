5fc158ee5624eccf3fa4050df5c1f4cb6b8feed9
Qingshuo SONG <songqsh@gmail.com>
2022-03-09 22:26:38 -0500

    func

--- src/call_functions.py
+++ src/call_functions.py
@@ -1,6 +1,6 @@
-from myfunctions import fall_dist, plus_two
+from myfunctions import fall_dist, plus_four
 
 out1 = fall_dist(3)
-out2 = plus_two(3)
+out2 = plus_four(3)
 
 print(out1, out2)
--- src/myfunctions.py
+++ src/myfunctions.py
@@ -8,3 +8,7 @@ def plus_two(n):
 def fall_dist(t, g=9.81):
     d = 0.5 * g * t ** 2
     return d
+
+
+def plus_four(n: int) -> int:
+    return plus_two(plus_two(n))
